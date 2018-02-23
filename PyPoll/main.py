f23f13e14b24a9ed9a8e703bd18b8048d5760ee0
indranik <35150683+indranik@users.noreply.github.com>
2018-02-23 15:23:00 -0800

    Working on the second challenge

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,20 +10,35 @@ import csv
 #candidates = {"name": "Candidate", "Votes": 0}
 #listResults=({}) List of candidates
 
-listResults = ()
+listResults = []
 numVotes = 0
 
-with open("election_data2.csv","r")as csvfile:
+with open("election_data_2.csv","r")as csvfile:
     csvreader = csv.reader(csvfile)
     next(csvreader)
     for line in csvreader:
+        candidates={}
+        
+        numVotes = numVotes+1
         if len(listResults) == 0:
             #This means we are reading the first row with data.
             candidates={"name": line[2],"Votes":1}
-            listResults = (candidates)
+            listResults.append(candidates)
         else:
-            candiatefound = false
-            for dicts in listResults:
-                if
+            candiatefound = False
+            for CDict in (listResults):
+                if CDict["name"] == line[2]: 
+                    candiatefound = True
+                    #increase the his or her vote count by 1
+                    CDict["Votes"] =  CDict["Votes"] + 1
+                    break
+                
+            if(candiatefound==False):
+                #Create a new dictonary for this candidate
             
-            
+                     candidates={"name": line[2],"Votes":1}
+                     listResults.append(candidates)
+                     
+print (numVotes)                
+print(listResults)
+           
